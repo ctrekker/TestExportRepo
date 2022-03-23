140bb4aee87bd0476634508e3d35790067d3ac57
Connor Burns <ctrekker4@gmail.com>
2022-03-22 20:07:47 -0600

    Update notebooks to 0.18.4

--- Kinda Binary Fourier Series.jl
+++ Kinda Binary Fourier Series.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.18.1
+# v0.18.4
 
 using Markdown
 using InteractiveUtils
@@ -67,7 +67,7 @@ PLUTO_PROJECT_TOML_CONTENTS = """
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.7.1"
+julia_version = "1.7.2"
 manifest_format = "2.0"
 
 [deps]
--- Rule 2530.jl
+++ Rule 2530.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.18.1
+# v0.18.4
 
 using Markdown
 using InteractiveUtils
@@ -89,7 +89,7 @@ Images = "~0.25.1"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.7.1"
+julia_version = "1.7.2"
 manifest_format = "2.0"
 
 [[deps.AbstractFFTs]]
